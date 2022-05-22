41eea7efad0383013bae6bb733b9cce07ce1871a
rachell <dazzlemevanity@gmail.com>
2022-05-22 19:13:29 -0400

    commit tree 5bede805fbed9c7abb716bdb0bbbb71647bc1e62

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
 
-// Write your code here
+let random = 5
+random > 0
+
+let num3 = 19
+let num4 = 5
+let mod = num3 % num4
+
+let max = 20 
+max > 10
